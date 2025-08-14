f31d256576726904249dba943a378c6c0e3f629b
bharathganji <ganjisai19@gmail.com>
2025-08-14 23:13:17 +0530

    fix: Refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import react from "@vitejs/plugin-react";
 import { URL, fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/", // Ensure proper base path for deployment
-  publicDir: "public", // Explicit public directory
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: "/", // Ensure proper base path for deployment
+    publicDir: "public", // Explicit public directory
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
